fa32f1eeeda58d2e2096c3d9bd731f23480c0a68
renhl <825156683@qq.com>
2017-08-09 10:20:26 +0800

    多重继承

--- __slots__.py
+++ __slots__.py
@@ -16,3 +16,37 @@ except AttributeError as e:
 g = SubStu()                 # 创建子类的实例
 g.age = 25                   # 动态创建属性age， __slots__ 限制属性对子类不起作用
 print('年龄',g.age)
+
+
+#　　　多重继承
+
+
+#生父
+class Father():
+
+    def func(self):
+        print('生父打儿子')
+
+#隔壁老王
+class LaoWang():
+
+    def func(self):
+        print('老王打儿子')
+    def func1(self):
+        print('跟你妈说明天下午我会来')
+#继父
+class StepFather():
+
+    def func(self):
+        print('继父打儿子')
+    def func1(self):
+        print('我还会打你妈')
+
+#神秘人
+class Mysterious(Father,StepFather,LaoWang):         # 父类存在相同函数，按（）内顺序继承
+    pass
+
+##让我们看看到底谁打了儿子
+s=Mysterious()
+s.func()
+s.func1()
